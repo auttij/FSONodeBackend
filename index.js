44700586089c24a7e1197ae50169f4fc93f3558b
auttij1 <juhana.autti@gmail.com>
2021-12-29 16:39:08 +0200

    removed old unused persons-list

--- index.js
+++ index.js
@@ -22,29 +22,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 	skip: function (req, res) { return req.method !== "POST"}
 }))
 
-persons = [
-	{
-		id: 1,
-		name : "Arto Hellas",
-		number: "040-123456"
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523"
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345"
-	},
-	{
-		id: 4,
-		name: "Mary Poppendick",
-		number: "39-23-6423122"
-	}
-]
-
 app.get('/info', (req, res) => {
 	const date_ob = new Date();
 	Person.find({}).then(persons => {
